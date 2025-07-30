92550c15dfa31a0d52d7b69db7f353b46b91432e
Brine150 <michaelbrine640@gmail.com>
2025-07-30 09:43:46 -0700

    index updade

--- js/sports.js
+++ js/sports.js
@@ -1,1 +1,24 @@
- 
+ const account = document.getElementById('account');
+const login = document.getElementById('login')
+const register = document.getElementById('register')
+let backtoregister = document.getElementById('backToRegister')
+let backtologin = document.getElementById('backToLogin')
+         
+
+account.onclick = function () {
+    // alert( "hi");
+    login.classList.toggle('swapDisplay');
+    register.classList.remove('swapDisplay');
+}
+
+backtoregister.onclick = function () {
+ login.classList.remove('swapDisplay');
+  register.classList.toggle('swapDisplay');
+}
+
+backtologin.onclick = function (){
+    register.classList.remove('swapDisplay');
+    login.classList.toggle('swapDisplay');
+}
+
+
